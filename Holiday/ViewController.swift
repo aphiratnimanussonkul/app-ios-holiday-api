9d7e3a69e433a8d8e1d8f2171a027e7cb16ea867
Aphirat Nimanussonkul <tata@odds.team>
2020-02-07 11:13:37 +0700

    Initial Commit

---
+++ Holiday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Holiday
+//
+//  Created by ODDS on 7/2/2563 BE.
+//  Copyright © 2563 ODDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HolidayTests/HolidayTests.swift
@@ -0,0 +1,34 @@
+//
+//  HolidayTests.swift
+//  HolidayTests
+//
+//  Created by ODDS on 7/2/2563 BE.
+//  Copyright © 2563 ODDS. All rights reserved.
+//
+
+import XCTest
+@testable import Holiday
+
+class HolidayTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
